6e1ad45750bd2b92d68b4189b4e5041b29ab05c3
Merrill <andrewjmarsden98@gmail.com>
2026-06-15 22:31:08 -0400

    commited

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,4 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: 'maxyf',       
-  host: 'localhost',
-  database: 'golden_cup',
-  password: '1237Kittles@',        
-  port: 5432,
-});
-
 export default pool;
